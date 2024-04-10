f735cb25d3b2098c747c31fdbbc366f038c7ff0c
Emhope <michailopalev02@gmail.com>
2024-04-11 00:14:32 +0300

    add memory buffer

---
+++ memory_buffer.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import random
+
+
+class MemoryBuffer:
+    def __init__(self, size):
+        self.data = []
+        self.size = size
+        self.updates = 0
+    
+    def add(self, item):
+        self.updates += 1
+        if len(self.data) == self.size:
+            self.data.pop(0)
+        self.data.append(item)
+    
+    def clear(self):
+        self.data = []
+    
+    def sample(self):
+        self.updates = 0
+        return random.sample(self.data, len(self.data))
+    
+    def is_fresh(self):
+        return self.updates > self.size
+    
